9e0159775e55ab5eb3d1e056998fc19104f0008b
Alex Ruiz <alruiz@google.com>
2013-02-21 23:42:56 -0800

    Added ability to check if an exception message contains certain strings.

--- src/main/java/org/fest/test/ExpectedException.java
+++ src/main/java/org/fest/test/ExpectedException.java
@@ -14,8 +14,11 @@
  */
 package org.fest.test;
 
+import java.util.Arrays;
+
 import javax.annotation.Nonnull;
 
+import org.junit.internal.matchers.TypeSafeMatcher;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -50,5 +53,24 @@ public class ExpectedException implements TestRule {
 
   public void expectMessage(@Nonnull String message) {
     delegate.expectMessage(message);
+  }
+
+  public void expectMessageToContain(final @Nonnull String...strings) {
+    delegate.expectMessage(new TypeSafeMatcher<String>() {
+      @Override
+      public void describeTo(org.hamcrest.Description description) {
+        description.appendText("containing: " + Arrays.toString(strings));
+      }
+
+      @Override
+      public boolean matchesSafely(String item) {
+        for (String s : strings) {
+          if (!item.contains(s)) {
+            return false;
+          }
+        }
+        return true;
+      }
+    });
   }
 }
